36bac9a49e5f37aed79acdc1df4d390f08409ec2
tymo.dekock <tymo.dekock>
2017-03-06 12:57:29 +0100

    sprite yoshi added!

--- playGame.js
+++ playGame.js
@@ -6,7 +6,10 @@ MyGame.playGameState.prototype = {
 
   create: function() {
     
-      this.background = game.add.tileSprite(0, 0, 800, 600, "sky");
+      this.background = game.add.tileSprite(0, 0, 600, 800, "sky");
+      this.yoshi = this.add.sprite(game.world.centerX, game.world.centerY +100, 'yoshi');
+      this.yoshi.animations.add('ani', [0,1,2,3]);
+      
     
       
 
@@ -14,6 +17,7 @@ MyGame.playGameState.prototype = {
   
   update: function() {
       this.background.tilePosition.y += 2;
+      this.yoshi.animations.play('ani', 6, true, false);
      
   }
 
