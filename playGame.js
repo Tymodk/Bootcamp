138d00f08c2bc1cf6fbaf67389dcfd28e2ab180e
tymo.dekock <tymo.dekock>
2017-03-06 12:26:19 +0100

    added scroll

--- playGame.js
+++ playGame.js
@@ -6,14 +6,14 @@ MyGame.playGameState.prototype = {
 
   create: function() {
     
-    
+      this.background = game.add.tileSprite(0, 0, 800, 600, "sky");
     
       
 
   },
   
   update: function() {
-      
+      this.background.tilePosition.y += 2;
      
   }
 
