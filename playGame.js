0bb5ec01cedffc300c206fb91b94cc4ec8985607
Woozsh Laptop <jordy.pereira@hotmail.com>
2017-03-06 13:19:13 +0100

    half movement

--- playGame.js
+++ playGame.js
@@ -6,16 +6,18 @@ MyGame.playGameState.prototype = {
 
   create: function() {
 
-    
-      this.background = game.add.tileSprite(0, 0, 600, 800, "sky");
-      this.yoshi = this.add.sprite(game.world.centerX, game.world.centerY +100, 'yoshi');
-      this.yoshi.animations.add('ani', [0,1,2,3]);
-      
-    
-    
 
-      //Variable keyboard keys
-      cursors = game.input.keyboard.createCursorKeys();
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+
+      this.background = game.add.tileSprite(0, 0, 600, 800, "sky");
+      //Add Player
+      this.yoshi = this.add.sprite(game.world.centerX, game.world.centerY +100, 'yoshi');
+      //Add Player Animations
+      this.yoshi.animations.add('ani', [0,1,2,3]);
+
+      this.yoshi.anchor.setTo(0.5, 0.5);
+      game.physics.enable(this.yoshi, Phaser.Physics.ARCADE);
+
 
 
 
@@ -24,25 +26,20 @@ MyGame.playGameState.prototype = {
   update: function() {
       this.background.tilePosition.y += 2;
       this.yoshi.animations.play('ani', 6, true, false);
-     
 
-
-      //Move left and right
-      if (cursors.left.isDown)
+      //Move Player
+      if (game.input.onDown)
       {
-          //  Move to the left
-          this.yoshi.body.velocity.x = -150;
-
-          //player.animations.play('left');
-      }
-      else if (cursors.right.isDown)
-      {
-          //  Move to the right
-          this.yoshi.body.velocity.x = 150;
-
-          //player.animations.play('right');
+        movePlayer();
       }
 
   }
 
+  function movePlayer() {
+    var x = game.input.mousePointer.x;
+    var y = game.input.mousePointer.y;
+
+    this
+
+  }
 }
