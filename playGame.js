a3e24d9569c42f75cb695b2d11444f4d6fa1983d
Woozsh Laptop <jordy.pereira@hotmail.com>
2017-03-06 12:45:17 +0100

    changed image names

--- playGame.js
+++ playGame.js
@@ -5,16 +5,37 @@ MyGame.playGameState.prototype = {
 
 
   create: function() {
-    
+
+      //Sky background
       this.background = game.add.tileSprite(0, 0, 800, 600, "sky");
-    
-      
+      //Init Player
+      this.player = game.add.sprite(32, 32, 'yoshi');
+
+
+      //Variable keyboard keys
+      cursors = game.input.keyboard.createCursorKeys();
+
 
   },
-  
+
   update: function() {
       this.background.tilePosition.y += 2;
-     
+
+      //Move left and right
+      if (cursors.left.isDown)
+      {
+          //  Move to the left
+          player.body.velocity.x = -150;
+
+          player.animations.play('left');
+      }
+      else if (cursors.right.isDown)
+      {
+          //  Move to the right
+          player.body.velocity.x = 150;
+
+          player.animations.play('right');
+      }
   }
 
 }
--- preload.js
+++ preload.js
@@ -7,9 +7,11 @@ MyGame.preloadState.prototype = {
 
   preload: function() {
 
-    
+
     this.load.image('sky', 'assets/background-tile.jpg');
-    
+    this.load.spritesheet('yoshi', 'assets/mario-yoshi-cape.png', 32, 48);
+
+
   },
   create: function() {
     this.state.start('playGame');
