5b9fef77aa29fcf3ebc3be81ad7f97742e25a304
Woozsh Laptop <jordy.pereira@hotmail.com>
2017-03-07 10:05:51 +0100

    collide enemies

--- playGame.js
+++ playGame.js
@@ -6,7 +6,9 @@ var fireballs;
 var lastFireballFired;
 var fireDelay = 400;
 var fireballSpeed = 250;
+
 var yoshiSpeed = 200;
+var koopas;
 
 
 MyGame.playGameState = function (game) {};
@@ -43,13 +45,17 @@ MyGame.playGameState.prototype = {
       //Score
       scoreText = game.add.text( 4, game.height - 32, 'score: 0',{font: 'Pixel' ,fontSize: '28px', fill: '#fff'});
 
-
-      
-      
+      //Fireballs
       fireballs = game.add.group();
       fireballs.enableBody = true;
-      
+
       this.generateFireball();
+
+      //Enemies
+      koopas = game.add.group();
+      koopas.enableBody = true;
+
+      this.generateKoopa();
 
   },
   update: function() {
@@ -57,7 +63,9 @@ MyGame.playGameState.prototype = {
       score += 1;
       scoreText.text = 'score: ' + score;
       this.yoshi.animations.play('ani', 6, true, false);
-      
+
+      game.physics.arcade.overlap(fireballs, koopas, destroyEnemy, null, this);
+
 
       if (Phaser.Rectangle.contains(this.yoshi.body, game.input.x, game.input.y))
         {
@@ -66,33 +74,44 @@ MyGame.playGameState.prototype = {
       else{
         game.physics.arcade.moveToPointer(this.yoshi, yoshiSpeed);
       }
-      
+
       if(game.time.now > (this.lastFireballFired + fireDelay))
         {
             this.generateFireball();
-    }
-  },
-    
-generateFireball: function() {
-    
-    
+        }
 
+  },
+
+generateFireball: function() {
     var fireball = fireballs.create(this.yoshi.position.x-10, this.yoshi.position.y-30, 'fireball-mini');
 
-      fireball.animations.add('spin', [0,1,2,3]);
-      fireball.animations.play('spin', 8, true, false);
-      game.physics.enable(fireball, Phaser.Physics.ARCADE);
-      fireball.events.onOutOfBounds.add(function(){
-    fireball.kill();
-      });
+    fireball.animations.add('spin', [0,1,2,3]);
+    fireball.animations.play('spin', 8, true, false);
+    game.physics.enable(fireball, Phaser.Physics.ARCADE);
+    fireball.events.onOutOfBounds.add(function(){
+      fireball.kill();
+    });
     fireball.checkWorldBounds = true;
-      fireball.body.velocity.y = - fireballSpeed;
+    fireball.body.velocity.y = - fireballSpeed;
     this.lastFireballFired = game.time.now;
 
+  },
+
+generateKoopa: function() {
+    var koopa = koopas.create(39, 46, 'koopa');
+
+    koopa.animations.add('koopa-ani', [0,1,2,3,4,5,6,7,8,9]);
+    koopa.animations.play('koopa-ani', 10, true, false);
+    game.physics.enable(koopa, Phaser.Physics.ARCADE);
+    koopa.checkWorldBounds = true;
+    // koopa.body.velocity.y =  fireballSpeed;
+
+  },
+
+  destroyEnemy: function(fireball, koopa) {
+      koopa.kill();
+      fireball.kill();
     }
 
-       
-    
+
 }
-
-
--- preload.js
+++ preload.js
@@ -6,19 +6,22 @@ MyGame.preloadState.prototype = {
 
     preload: function() {
         this.load.image('sky', 'assets/background-tile.jpg');
-        
+
         this.load.spritesheet('yoshi', 'assets/yoshi.png', 27, 50, 4);
         this.load.spritesheet('fireball', 'assets/mario_fireball.png', 27, 50, 4);
         this.load.spritesheet('fireball-mini', 'assets/fireballs.png', 19, 20, 4);
         this.load.spritesheet('fireball-big', 'assets/bigfireballs.png', 16, 16, 2);
         this.load.spritesheet('fireball-bigger', 'assets/biggerfireballs.png', 21, 21, 2);
-        
+
+        this.load.spritesheet('koopa', 'assets/koopas.png', 39, 46, 10);
+
+
         this.load.image('title', 'assets/titlescreen_title.jpg');
         this.load.image('startGame', 'assets/button_startGame.png');
         this.load.image('store', 'assets/button_store.png');
         this.load.image('options', 'assets/button_options.png');
         this.load.image('exit', 'assets/button_exit.png');
-        
+
         this.load.audio('water', 'assets/underwater.mp3');
     },
 
@@ -27,7 +30,7 @@ MyGame.preloadState.prototype = {
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        
+
         this.state.start('titlescreen');
     }
 }
