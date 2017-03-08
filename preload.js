7f852c278b16a0763ce6c52fbbc948bc5309d195
nixaun <lenn_peeters@yahoo.com>
2017-03-08 16:40:17 +0100

    cleanup index, title, options & preload

--- preload.js
+++ preload.js
@@ -1,18 +1,18 @@
+//initiating game
 var MyGame = {};
-
+//initiating state
 MyGame.preloadState = function(game) {};
-
 MyGame.preloadState.prototype = {
-
     preload: function() {
+        //loading backgrounds assets
         this.load.image('sky', 'assets/background-tile.jpg');
         this.load.image('scoreBackground','assets/scoreBackground.jpg');
         this.load.image('sky-boss', 'assets/background-tile-boss.png');
-        
+        //loading countdown
         this.load.image('ready', 'assets/preGame_ready.png');
         this.load.image('set', 'assets/preGame_set.png');
         this.load.image('go', 'assets/preGame_go.png');
-        
+        //loading game assets
         this.load.spritesheet('yoshi', 'assets/yoshi.png', 27, 50, 4);
         this.load.spritesheet('fireball', 'assets/mario_fireball.png', 27, 50, 4);
         this.load.spritesheet('fireball-mini', 'assets/fireballs2.png', 28, 29, 4);
@@ -24,10 +24,9 @@ MyGame.preloadState.prototype = {
         this.load.spritesheet('coin', 'assets/coin2.png', 32 , 32 , 4 );
         this.load.spritesheet('goomba', 'assets/Goomba2.png', 67 ,44,3);
         this.load.spritesheet('star', 'assets/star.png', 16, 16, 3);
-
         this.load.spritesheet('boo', 'assets/boo.png', 300, 300, 2);
         this.load.image('bullet', 'assets/bullet_bill.png');
-
+        // loading options assets
         this.load.image('title', 'assets/titlescreen_title.jpg');
         this.load.image('gameOverTitle', 'assets/gameOver.png');
         this.load.image('startGame', 'assets/button_startGame.png');
@@ -37,25 +36,19 @@ MyGame.preloadState.prototype = {
         this.load.image('retry', 'assets/button_retry.png');
         this.load.image('buttonYes', 'assets/button_yes.png');
         this.load.image('buttonNo', 'assets/button_no.png');
-
+        //loading sound assets
         this.load.audio('water', 'assets/underwater.mp3');
         this.load.audio('coinSound', 'assets/smw_coin.wav');
         this.load.audio('blockSound', 'assets/smw_message_block.wav');
         this.load.audio('fireSmallSound', 'assets/smw_bowser_fire.wav');
         this.load.audio('deathSound', 'assets/smw_stomp.wav');
         this.load.audio('gameover', 'assets/smw_game_over.wav');
-
-
     },
-
-
     create: function() {
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         music = game.add.audio('water');
-        
-
         this.state.start('titlescreen');
     }
 }
