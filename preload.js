046ccc04adc164103af3cf661d6f934b961d24d6
tymo.dekock <tymo.dekock>
2017-03-08 12:20:32 +0100

    sprites bigger

--- preload.js
+++ preload.js
@@ -15,7 +15,7 @@ MyGame.preloadState.prototype = {
         
         this.load.spritesheet('yoshi', 'assets/yoshi.png', 27, 50, 4);
         this.load.spritesheet('fireball', 'assets/mario_fireball.png', 27, 50, 4);
-        this.load.spritesheet('fireball-mini', 'assets/fireballs.png', 19, 20, 4);
+        this.load.spritesheet('fireball-mini', 'assets/fireballs2.png', 28, 29, 4);
         this.load.spritesheet('fireball-big', 'assets/bigfireballs.png', 16, 16, 2);
         this.load.spritesheet('fireball-bigger', 'assets/biggerfireballs.png', 21, 21, 2);
         this.load.spritesheet('explosion', 'assets/explosion.png', 31 ,48,9);
